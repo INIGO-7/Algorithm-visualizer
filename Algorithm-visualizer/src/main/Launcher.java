f4d56017ced4e296c4c315ed14b26751c3360f08
Iñigo Fernandez <inigofernandezsopena@gmail.com>
2022-04-30 18:20:02 +0200

    Grid made for visualizations

--- Algorithm-visualizer/src/main/Launcher.java
+++ Algorithm-visualizer/src/main/Launcher.java
@@ -3,7 +3,7 @@ package main;
 public class Launcher {
 
 	public static void main(String[] args) {
-		Main main = new Main(400, 300, "Algorithm Visualizer");
+		Main main = new Main(800, 600, "Algorithm Visualizer");
 		main.start();
 	}
 	
--- Algorithm-visualizer/src/main/states/ProgramState.java
+++ Algorithm-visualizer/src/main/states/ProgramState.java
@@ -10,8 +10,32 @@ public class ProgramState extends State{
 
     public void render(Graphics g) {
 
-        g.setColor(Color.RED);
-        g.fillRect(50, 50, 200, 200);
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, 800, 600);
+
+        //drawing the grid:
+
+        g.setColor(new Color(150, 150, 150));
+
+        // X axis
+
+        for(int x = 0; x < 800; x += 25){
+            g.drawLine(x, 0, x, 600);
+        }
+
+        // Y axis
+
+        for(int y = 0; y < 600; y+=25){
+            g.drawLine(0, y, 800, y);
+        }
+
+        //boundaries
+
+        g.setColor(new Color(150, 150, 150));
+        g.drawLine(0, 0, 800, 0);
+        g.drawLine(0, 599, 800, 599);
+        g.drawLine(0, 0, 0, 800);
+        g.drawLine(799, 0, 799, 599);
 
     }
 
